94f323ee145bda16fa513ad13c42f4b3282a042f
xihale <xihale.top@qq.com>
2024-04-04 21:16:07 +0800

    fix: min-width

--- src/ui/PoemList.tsx
+++ src/ui/PoemList.tsx
@@ -1,7 +1,6 @@
 import getPoems from "@/apis/getPoems"
 
 import styles from  "./PoemList.module.scss"
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function PoemList(){
