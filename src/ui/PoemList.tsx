cf01c28bf886218bf5cf5c23082e9248b5e84ac3
xihale <xihale.top@qq.com>
2024-04-05 08:57:32 +0800

    fix: made br out of span

--- src/ui/PoemList.tsx
+++ src/ui/PoemList.tsx
@@ -10,13 +10,13 @@ export default function PoemList(){
   return <>
     <div className={styles.container}>
       {poems.map((d)=>
-        <Link href={`/poem/${d.id}`} key={d.id} className={styles.card}>
+        <Link href={`/${d.id}`} key={d.id} className={styles.card}>
           <div className={styles.meta}>
             <div className={styles.title}>{d.title}</div>
             <div>{d.extra}</div>
           </div>
           <div className={styles.brief}>
-            {d.brief.map((b,i)=>(<span key={i}>{b}<br/></span>))}
+            {d.brief.map((b,i)=>(<><span key={i}>{b}</span><br/></>))}
           </div>
           <span>......</span>
         </Link>
